90ff4e07cac5d2daf1744e030d91c101a99ecf7a
Jonas Teuwen <j.teuwen@nki.nl>
2023-01-09 21:22:45 +0100

    Create facts

---
+++ roles/facts/files/create_fstab.py
@@ -0,0 +1,16 @@
+import sys
+import json
+
+args = ''.join(sys.argv[1:])
+folders = [_.strip() for _ in args.split(",") if _ != ""]
+
+lines = []
+for folder in folders:
+    lines.append(f"rhea:/project-pool/projects/{folder} /projects/{folder} nfs rsize=524288,wsize=524288,vers=3,timeo=30,intr 0 0")
+
+project_folders = []
+for folder in folders:
+    project_folders.append(f"/projects/{folder}")
+
+output = {"projects_fstab": lines, "projects_folders": project_folders}
+print(json.dumps(output, indent=2))
